44e25f6b476d6cc640a11940f0002fc47e9eeec9
Aurlyo <38880625+Aurlyo@users.noreply.github.com>
2018-05-01 20:24:23 +0200

    Update Tarot.java

--- Project/jeu/Tarot.java
+++ Project/jeu/Tarot.java
@@ -15,22 +15,22 @@ import objets.Paquet;
 public class Tarot {
 
 	static byte nbJoueur;					// Le nombre de Joueurs.
-	static byte nbDistribution = 0; 		// Le nombre de distribution effectué (Debug).
+	static byte nbDistribution = 0; 			// Le nombre de distribution effectué (Debug).
 	
 	public static void main(String[] args) {
 		
 		Debug.initConsole(); 				// Affiche une fenêtre console, avec entrée clavier.
 		choixNombreDeJoueur(); 				// Choix du nombre de joueurs.
 		
-		Jeu Tarot = new Jeu(nbJoueur); 		// Création du jeu selon ce nombre de joueurs.
+		Jeu Tarot = new Jeu(nbJoueur); 			// Création du jeu selon ce nombre de joueurs.
 		Tarot.creationJoueurs(); 			// Création des joueurs.
 		
-		while (Joueur.prise != true) {		// Boucle tant que le joueur ne prends pas.
+		while (Joueur.prise != true) {			// Boucle tant que le joueur ne prends pas.
 			nbDistribution++;
-			reset(); 		 	 			// Remet tout a zero.
+			reset(); 		 	 	// Remet tout a zero.
 			Tarot.distribution();			// Distribue les cartes.
-			Tarot.affichage();				// Affiche les pseudos et score des joueurs.
-			Tarot.mise();					// Propose au joueur de miser.
+			Tarot.affichage();			// Affiche les pseudos et score des joueurs.
+			Tarot.mise();				// Propose au joueur de miser.
 		}
 
 		System.out.println("Vous avez fait : " + nbDistribution + " distribtutions avant de prendre ! \n"); //indique le nombre de fois que la boucle a été exécuté.
@@ -53,11 +53,11 @@ public class Tarot {
 		String[] nb = {"3 joueurs", "4 joueurs"};
 	    nbJoueur = (byte) JOptionPane.showOptionDialog(null,  //Permet de choisir le nombre de joueurs.
 	    	      "A combien voulez vous jouer ?",            //Message de la fenêtre
-	    	      "2D > 3D",								  //Titre de la fenêtre
-	    	      JOptionPane.YES_NO_CANCEL_OPTION,			  //permet de donner un int
-	    	      JOptionPane.QUESTION_MESSAGE,				  //précise qu'on attends une réponse de l'utilisateur
+	    	      "2D > 3D",				  //Titre de la fenêtre
+	    	      JOptionPane.YES_NO_CANCEL_OPTION,		  //permet de donner un int
+	    	      JOptionPane.QUESTION_MESSAGE,		  //précise qu'on attends une réponse de l'utilisateur
 	    	      null,
-	    	      nb,										  //Liste de référence
+	    	      nb,					  //Liste de référence
 	    	      nb[0]);
 	    
 	    if (nbJoueur == 0) { //Transforme la réponse en obtenu, en chiffre plus significatifs.
